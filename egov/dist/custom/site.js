7597f44f10c1821221cdd6c1b6c77b0928737aef
mdot-ocx <146029629+mdot-ocx@users.noreply.github.com>
2025-01-21 10:43:38 -0500

    Add files via upload

---
+++ egov/dist/custom/site.js
@@ -0,0 +1,15 @@
+(function ($) {
+
+    /*
+        Self-invoking function to contain scope. Receives $ as the global jQuery function.
+    */
+
+    mdgov.ready(function () {
+        /*
+            Add your site's JS code here. When working with any MDGOV template module, be sure the API
+            is ready by keeping code within this "mdgov.ready" callback.
+            NOTE: this.modules contains template module objects.
+        */
+    });
+
+})(jQuery);
